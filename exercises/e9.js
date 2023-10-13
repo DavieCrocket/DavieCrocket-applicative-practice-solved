e0d394ee7c7a840013f406415eb039b262a24430
Doug Stensrud <dwsmitpython@gmail.com>
2023-10-13 02:50:32 -0700

    pass test-9

--- exercises/e9.js
+++ exercises/e9.js
@@ -1,14 +1,20 @@
-import { data } from "../data/data";
+import { data } from "../data/data.js";
 
 // SPACE DATA EXERCISE 9
 // Return the data object of the planet "Earth"
 // Return example: { key1: value1, key2: value2, ... , keyN: valueN }
 
 export function getEarthData(data) {
-  // Your code goes here...
+  return data.planets.filter(function(planet) {
+    if (planet.id === 'earth') {
+      const myPlanet = data.planets[6];
+      return myPlanet;
+    }
+  })[0];
 }
 
 
+getEarthData(data);
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-9"
